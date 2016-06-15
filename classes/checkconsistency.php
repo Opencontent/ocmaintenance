c33ce1936e7ad65f85c650f4ff83d92d6c913d38
Luca Realdi <lr@opencontent.it>
2016-06-16 00:50:56 +0200

    checkconsitency refactor

---
+++ classes/checkconsistency.php
@@ -0,0 +1,241 @@
+<?php
+
+class OcCheckConsistency
+{
+    private static $instance;
+
+    protected $db;
+
+    protected $verbose = false;
+
+    /**
+     * @var ezcConsoleOutput
+     */
+    protected $output;
+
+    protected $outputOptions = array();
+
+    public static function instance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new OcCheckConsistency();
+        }
+
+        return self::$instance;
+    }
+
+    protected function __construct()
+    {
+        $this->db = eZDB::instance();
+    }
+
+    protected function log($string)
+    {
+        eZLog::write($string, 'checkconsistency.log');
+    }
+
+    public function verbose()
+    {
+        $this->verbose = true;
+        $this->output = new ezcConsoleOutput();
+        $this->output->formats->success->color = 'green';
+        $this->output->formats->failure->color = 'red';
+
+        $this->outputOptions = array(
+            'successChar' => $this->output->formatText( '+', 'success' ),
+            'failureChar' => $this->output->formatText( '-', 'failure' ),
+        );
+
+
+    }
+
+    public function checkObjectsWithoutAttributes($doFix = false)
+    {
+        $objects = $this->db->arrayQuery(
+            "SELECT id, contentclass_id, current_version FROM ezcontentobject"
+        );
+
+        if ($this->output) {
+            $status = new ezcConsoleStatusbar($this->output, $this->outputOptions);
+        }
+
+        $i = 0;
+        foreach ($objects as $item) {
+            $hasAttribute = $this->db->arrayQuery(
+                "SELECT contentobject_id FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id']
+            );
+
+            if (empty( $hasAttribute )) {
+
+                if ($doFix) {
+                    $this->log('Delete ezcontentobject.id ' . $item['id']);
+                    $this->db->begin();
+                    $this->db->query("DELETE FROM ezcontentobject WHERE ezcontentobject.id = " . $item['id']);
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject_name WHERE ezcontentobject_name.contentobject_id = " . $item['id']
+                    );
+                    if ($item['contentclass_id'] == 4) {
+                        $this->db->query("DELETE FROM ezuser WHERE ezuser.contentobject_id = " . $item['id']);
+                    }
+                    $this->db->commit();
+                } else {
+                    if (isset($status)) $status->add(0);
+                }
+                $i++;
+            }
+            if (isset($status)) $status->add(1);
+        }
+
+        if ($this->output){
+            $this->output->outputLine();
+            $this->output->outputLine( 'Successes: ' . $status->getSuccessCount() . ', Failures: ' . $status->getFailureCount() );
+        }
+
+        return $i;
+    }
+
+    public function checkNodesConsistency($doFix = false)
+    {
+        $objects = $this->db->arrayQuery("SELECT id, name FROM ezcontentobject");
+
+        $count = 0;
+
+        if ($this->output) {
+            $status = new ezcConsoleStatusbar($this->output, $this->outputOptions);
+        }
+
+        foreach ($objects as $item) {
+
+            $hasNode = $this->db->arrayQuery(
+                "SELECT contentobject_id FROM ezcontentobject_tree WHERE contentobject_id = " . $item['id']
+            );
+
+            $hasTrashNode = $this->db->arrayQuery(
+                "SELECT contentobject_id FROM ezcontentobject_trash WHERE contentobject_id = " . $item['id']
+            );
+
+            if (empty( $hasNode ) && empty( $hasTrashNode )) {
+                if (isset($status)) $status->add(0);
+                $count++;
+            } else {
+                if (isset($status)) $status->add(1);
+            }
+        }
+
+        if ($doFix) {
+
+            foreach ($objects as $item) {
+                $hasNode = $this->db->arrayQuery(
+                    "SELECT contentobject_id FROM ezcontentobject_tree WHERE contentobject_id = " . $item['id']
+                );
+                $hasTrashNode = $this->db->arrayQuery(
+                    "SELECT contentobject_id FROM ezcontentobject_trash WHERE contentobject_id = " . $item['id']
+                );
+
+                if (empty( $hasNode ) && empty( $hasTrashNode )) {
+
+                    $this->log('Delete ezcontentobject.id ' . $item['id']);
+                    $this->db->begin();
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject WHERE id = " . $item['id']
+                    );
+
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject_name WHERE contentobject_id = " . $item['id']
+                    );
+
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject_link WHERE (from_contentobject_id = " . $item['id'] . " OR to_contentobject_id = " . $item['id'] . ")"
+                    );
+
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject_version WHERE contentobject_id = " . $item['id']
+                    );
+
+                    $this->db->query(
+                        "DELETE FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id']
+                    );
+
+                    $this->db->query(
+                        "DELETE FROM eznode_assignment WHERE contentobject_id = " . $item['id']
+                    );
+                    $this->db->commit();
+                }
+            }
+        }
+        if ($this->output){
+            $this->output->outputLine();
+            $this->output->outputLine( 'Successes: ' . $status->getSuccessCount() . ', Failures: ' . $status->getFailureCount() );
+        }
+
+        return $count;
+    }
+
+    public function checkObjectsWithoutCurrentVersion($doFix = false)
+    {
+        $resources = $this->db->arrayQuery("SELECT id, current_version, name FROM ezcontentobject");
+
+        if ($this->output) {
+            $status = new ezcConsoleStatusbar($this->output, $this->outputOptions);
+        }
+
+        $i = 0;
+        foreach ($resources as $item) {
+
+            //check if current_version has content attributes
+            $hasAttribute = $this->db->arrayQuery(
+                "SELECT contentobject_id FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id'] . " AND version = " . $item['current_version']
+            );
+
+            if (empty( $hasAttribute )) {
+                $previousCurrentVersion = $item['current_version'] - 1;
+                if ($previousCurrentVersion > 0) {
+                    if ($doFix) {
+                        $this->log('Update current_version in ezcontentobject.id ' . $item['id']);
+                        $this->db->begin();
+                        $this->db->query(
+                            "UPDATE ezcontentobject SET current_version = " . $previousCurrentVersion . " WHERE id = " . $item['id']
+                        );
+                        $this->db->commit();
+                    }
+                    if (isset($status)) $status->add(0);
+                } else {
+                    if (isset($status)) $status->add(1);
+                }
+                $i++;
+            }
+        }
+        if ($this->output){
+            $this->output->outputLine();
+            $this->output->outputLine( 'Successes: ' . $status->getSuccessCount() . ', Failures: ' . $status->getFailureCount() );
+        }
+
+        return $i;
+    }
+
+    public function checkUserWithoutLogin($doFix = false)
+    {
+        if ($this->output) {
+            $status = new ezcConsoleStatusbar($this->output, $this->outputOptions);
+        }
+        $resources = $this->db->arrayQuery("SELECT * FROM ezuser WHERE login = ''");
+        if ($doFix) {
+            foreach ($resources as $item) {
+
+                $this->log('Delete ezuser.contentobject_id ' . $item['id']);
+                $this->db->begin();
+
+                $this->db->query(
+                    "UPDATE ezcontentobject_attribute SET data_text='' WHERE contentobject_id = " . intval($item['contentobject_id']) . " AND data_type_string = 'ezuser'"
+                );
+                $this->db->query(
+                    "DELETE FROM ezuser WHERE contentobject_id = " . intval($item['contentobject_id'])
+                );
+                $this->db->commit();
+            }
+        }
+
+        return count($resources);
+    }
+
+}
--- cronjobs/check_consistency.php
+++ cronjobs/check_consistency.php
@@ -1,188 +1,58 @@
 <?php
 
-require_once( 'autoload.php' );
+/** @var $cli */
+/** @var $script */
 
-$ini = eZINI::instance('content.ini');
-$unpublishClasses = $ini->variable('UnpublishSettings', 'ClassList');
-$rootNodeIDList = $ini->variable('UnpublishSettings', 'RootNodeList');
-$UnpublishDateAttributes = $ini->variable('UnpublishSettings', 'Unpublish_field');
-$currrentDate = time();
-
-
-// Script for finding and handling content_objects that are not completely created
-// That may occur under some circumstances when using a database without translations enabled
-//
 // $doUpdate, true or false. Set to false for at dry test-run
 $doUpdate = true;
 
-//include_once( 'kernel/common/template.php' );
-//include_once( "lib/ezutils/classes/ezhttptool.php" );
-//include_once( 'lib/ezutils/classes/ezcli.php' );
-//include_once( 'kernel/classes/ezscript.php' );
-//include_once( 'lib/ezdb/classes/ezdb.php' );
-
-$cli =& eZCLI::instance();
-$cli->setUseStyles(true);
-$script =& eZScript::instance();
-$script->initialize();
-$db =& eZDB::instance();
 set_time_limit(0);
-$arrayResult1 = $db->arrayQuery(
-    "SELECT id, contentclass_id, current_version FROM ezcontentobject"
-);
-$message = "First checking for content objects that has no contentobject_attributes at all...\n";
-$cli->output($cli->stylize('cyan', $message), false);
 
-$i = 0;
-foreach ($arrayResult1 as $item) {
-    //check if object has no attributes of any version stored
-    $hasAttribute = $db->arrayQuery(
-        "SELECT contentobject_id FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id']
-    );
-
-    if (empty( $hasAttribute )) {
-        if ($doUpdate) {
-            $message = "Corrupt object, no attributes: " . $item['id'] . ". Deleting corrupt object with no attributes...\n";
-            $cli->output($cli->stylize('red', $message), false);
-            $db->query("DELETE FROM ezcontentobject WHERE ezcontentobject.id = " . $item['id']);
-            $db->query(
-                "DELETE FROM ezcontentobject_name WHERE ezcontentobject_name.contentobject_id = " . $item['id']
-            );
-            if ($item['contentclass_id'] == 4) {
-                $db->query("DELETE FROM ezuser WHERE ezuser.contentobject_id = " . $item['id']);
-            }
-        } else {
-            $message = "Corrupt object, no attributes: " . $item['id'] . ", current_version:" . $item['current_version'] . "\n";
-            $cli->output($cli->stylize('red', $message), false);
-        }
-        $i++;
-    }
-}
-$message = "Total corrupt objects with no attributes: " . $i . "\n";
-$cli->output($cli->stylize('red', $message), false);
-
-
-$arrayResult3 = $db->arrayQuery("SELECT id, name FROM ezcontentobject");
-
-$count = 0;
-$message = "Check nodes' consistency... ";
-$cli->output($cli->stylize('cyan', $message), false);
-foreach ($arrayResult3 as $item) {
-
-    $hasNode = $db->arrayQuery(
-        "SELECT contentobject_id FROM ezcontentobject_tree WHERE contentobject_id = " . $item['id']
-    );
-    $hasTrashNode = $db->arrayQuery(
-        "SELECT contentobject_id FROM ezcontentobject_trash WHERE contentobject_id = " . $item['id']
-    );
-    if (empty( $hasNode ) && empty( $hasTrashNode )) {
-        $count++;
-        echo "The node does not exist for the object: " . $item['name'] . ", ID " . $item['id'] . "\n";
-    }
-}
-$message = "Check completed. Affected objects:" . $count . "\n";
-$cli->output($cli->stylize('red', $message), false);
-
-
-if ($doUpdate) {
-    foreach ($arrayResult3 as $item) {
-        $hasNode = $db->arrayQuery(
-            "SELECT contentobject_id FROM ezcontentobject_tree WHERE contentobject_id = " . $item['id']
-        );
-        $hasTrashNode = $db->arrayQuery(
-            "SELECT contentobject_id FROM ezcontentobject_trash WHERE contentobject_id = " . $item['id']
-        );
-        if (empty( $hasNode ) && empty( $hasTrashNode )) {
-            $message = "The node does not exist for the object: " . $item['name'] . ", ID " . $item['id'] . "\n";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query("DELETE FROM ezcontentobject WHERE id = " . $item['id']);
-            $message = "Object removed; ";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query(
-                "DELETE FROM ezcontentobject_name WHERE contentobject_id = " . $item['id']
-            );
-            $message = "Object name removed; ";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query(
-                "DELETE FROM ezcontentobject_link WHERE (from_contentobject_id = " . $item['id'] . " OR to_contentobject_id = " . $item['id'] . ")"
-            );
-            $message = "Object links removed; ";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query(
-                "DELETE FROM ezcontentobject_version WHERE contentobject_id = " . $item['id']
-            );
-            $message = "Object versions removed; ";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query(
-                "DELETE FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id']
-            );
-            $message = "Object attributes removed" . "\n";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query("DELETE FROM eznode_assignment WHERE contentobject_id = " . $item['id']);
-            $message = "Nodes assignment removed" . "\n";
-            $cli->output($cli->stylize('cyan', $message), false);
-        }
-    }
+if (!class_exists('OcCheckConsistency')){
+    $script->shutdown(1, 'OcCheckConsistency class not found. Regenerate autoloads!');
 }
 
+$tool = OcCheckConsistency::instance();
 
-$arrayResult2 = $db->arrayQuery("SELECT id, current_version, name FROM ezcontentobject");
-$message = "Then checking for content objects that has contentobject_attributes, but not of the current_version...\n";
-$cli->output($cli->stylize('cyan', $message), false);
+if (!$isQuiet) $tool->verbose();
 
-$i = 0;
-foreach ($arrayResult2 as $item) {
-    //check if current_version has content attributes
-    $hasAttribute = $db->arrayQuery(
-        "SELECT contentobject_id FROM ezcontentobject_attribute WHERE contentobject_id = " . $item['id'] . " AND version = " . $item['current_version']
-    );
-
-    if (empty( $hasAttribute )) {
-        if ($doUpdate) {
-            $previousCurrentVersion = $item['current_version'] - 1;
-            $message = "Corrupt object: " . $item['id'] . ", current_version: " . $item['current_version'] . ". Setting back to version: " . $previousCurrentVersion . "\n";
-            $cli->output($cli->stylize('cyan', $message), false);
-            $db->query(
-                "UPDATE ezcontentobject SET current_version = " . $previousCurrentVersion . " WHERE id = " . $item['id']
-            );
-        } else {
-            $message = "Corrupt object: " . $item['id'] . ", current_version: " . $item['current_version'] . "\n";
-            $cli->output($cli->stylize('cyan', $message), false);
-        }
-        $i++;
-    }
+$cli->output();
+$cli->output("First checking for content objects that has no contentobject_attributes at all...");
+$cli->output();
+$count = $tool->checkObjectsWithoutAttributes();
+$cli->output("Affected objects: $count");
+if ($doUpdate && $count > 0) {
+    $cli->warning("Fixing... please wait");
+    $tool->checkObjectsWithoutAttributes(true);
 }
-$message = "Total objects withp wrong current_version: " . $i . "\n";
-$cli->output($cli->stylize('cyan', $message), false);
+$cli->output();
+$cli->output("Check nodes consistency");
+$cli->output();
+$count = $tool->checkNodesConsistency();
+$cli->output("Affected objects: $count");
+if ($doUpdate && $count > 0) {
+    $cli->warning("Fixing... please wait");
+    $tool->checkNodesConsistency(true);
+}
 
+$cli->output();
+$cli->output("Then checking for content objects that has contentobject_attributes, but not of the current_version");
+$cli->output();
+$count = $tool->checkObjectsWithoutCurrentVersion();
+$cli->output("Affected objects: $count");
+if ($doUpdate && $count > 0) {
+    $cli->warning("Fixing... please wait");
+    $tool->checkObjectsWithoutCurrentVersion(true);
+}
+
+$cli->output();
+$cli->output("Check users without login name");
+$cli->output();
+$count = $tool->checkUserWithoutLogin();
+$cli->output("Affected objects: $count");
+if ($doUpdate && $count > 0) {
+    $cli->warning("Fixing... please wait");
+    $tool->checkUserWithoutLogin(true);
+}
 
 $script->shutdown();
-
-
-/*
-foreach( $rootNodeIDList as $nodeID )
-{
-    $rootNode = eZContentObjectTreeNode::fetch( $nodeID );
-    $articleNodeArray = $rootNode->subTree( array( 'ClassFilterType' => 'include',
-                                                    'ClassFilterArray' => $unpublishClasses ) );
-    foreach ( $articleNodeArray as $articleNode )
-    {
-        $article = $articleNode->attribute( 'object' );
-        $dataMap = $article->attribute( 'data_map' );
-	
-	$unpublish_date = $UnpublishDateAttributes[$article->ClassIdentifier];
-        $dateAttribute = $dataMap[$unpublish_date];
-        if ( is_null( $dateAttribute ) )
-            continue;
-        $date = $dateAttribute->content();
-        $articleRetractDate = $date->attribute( 'timestamp' );
-        if ( $articleRetractDate > 0 && $articleRetractDate < $currrentDate )
-        {
-            // Clean up content cache
-            //include_once( 'kernel/classes/ezcontentcachemanager.php' );
-            eZContentCacheManager::clearContentCacheIfNeeded( $article->attribute( 'id' ) );
-            $article->removeThis( $articleNode->attribute( 'node_id' ) );
-        }
-    }
-}
-*/
